92c75b05491e7bc193a539759f1a4be3e64b14cb
Sze Yick <szeyick@gmail.com>
2013-07-24 21:11:02 +1000

    Delivery of project - HelloWorld
    Test delivery of initial project - HelloWorld.

---
+++ HelloWorld/src/com/dataStructures/helloWorld/main/HelloWorld.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.dataStructures.helloWorld.main;
+
+/**
+ * HelloWorld Test Program.
+ * 
+ * This is a basic program that demonstrates a very
+ * basic Java program.
+ * 
+ * @author szeyick
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
