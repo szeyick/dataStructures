bd0f669436f6ffbb4f16c22d299b97d8664d9e19
szeyick <szeyick@gmail.com>
2014-02-11 21:01:46 +1100

    Linked Lists Delivery
    Initial delivery for Linked Lists project.
    
    Does not contain any operational code, only project structure.

---
+++ LinkedLists/src/com/dataStructureExamples/linkedLists/main/Main.java
@@ -0,0 +1,18 @@
+package com.dataStructureExamples.linkedLists.main;
+
+/**
+ * Main class that runs the program.
+ * 
+ * @author szeyick
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+}
