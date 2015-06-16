6fd5ab96848ba14d9fb7ceaac80290f73b206c3b
Sze Yick <szeyick@gmail.com>
2015-06-16 23:06:07 +1000

    Updates project notes.

--- IntroductionToAlgorithmsAndDataStructures/README.md
+++ IntroductionToAlgorithmsAndDataStructures/README.md
@@ -1,1 +1,3 @@
 ## Introduction to Algorithms and Data Structures
+
+This project file contains the contents for the introductory notes for studying algorithms and data structures. 
--- IntroductionToAlgorithmsAndDataStructures/src/com/algorithmsAndDataStructures/main/Main.java
+++ IntroductionToAlgorithmsAndDataStructures/src/com/algorithmsAndDataStructures/main/Main.java
@@ -14,5 +14,6 @@ public class Main {
      */
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Testing Deploy");
     }
 }
